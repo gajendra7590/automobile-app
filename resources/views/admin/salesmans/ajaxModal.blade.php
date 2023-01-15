dff9ed8b54cd0c77a4cfdd614b8639f1c10cdff4
Gajendra Singh Pawar <gajendra.pawar@rsvrtech.com>
2023-01-15 12:20:37 +0530

    changes

--- resources/views/admin/salesmans/ajaxModal.blade.php
+++ resources/views/admin/salesmans/ajaxModal.blade.php
@@ -7,18 +7,18 @@
     <div class="row">
         <div class="form-group col-md-4">
             <label>Name</label>
-            <input type="text" class="form-control my-colorpicker1 colorpicker-element" placeholder="Name" required
+            <input type="text" class="form-control my-colorpicker1 colorpicker-element" placeholder="Name"
                 name="name" value='{{ isset($data) && $data ? $data->name : '' }}' />
         </div>
         <div class="form-group col-md-4">
             <label>Email</label>
-            <input type='email' class="form-control my-colorpicker1 colorpicker-element" placeholder="Email" required
+            <input type='text' class="form-control my-colorpicker1 colorpicker-element" placeholder="Email"
                 name="email" value="{{ isset($data) && $data ? $data->email : '' }}" />
         </div>
         <div class="form-group col-md-4">
             <label>Mobile Number</label>
             <input type='text' class="form-control my-colorpicker1 colorpicker-element" placeholder="Mobile Number"
-                required name="mobile_number" value="{{ isset($data) && $data ? $data->mobile_number : '' }}" />
+                name="mobile_number" value="{{ isset($data) && $data ? $data->mobile_number : '' }}" />
         </div>
         <div class="form-group col-md-12">
             <label>Status : </label>
